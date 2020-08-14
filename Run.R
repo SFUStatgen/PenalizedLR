acde5ce51fb6d8c2f903d391d8dc0d796e7e1bb6
Daisy Yu <67029201+daisyyu0130@users.noreply.github.com>
2020-08-14 16:39:01 -0700

    Running script for MCEM

--- Run.R
+++ Run.R
@@ -1,8 +1,7 @@
 library(dplyr)
 library(SNPknock)
 
-load("Input_dataset.RData")
-#load("MCEM_Multiple-SNP.RData")
+load("Input_dataset_1000GP.RData")
 load("MCEM_Single-SNP.RData")
 
 args <- commandArgs()
@@ -12,11 +11,12 @@ print(n)
 m=4
 K=10
 set.seed(2020*n+K)
-#sim_data=simUnmatched(n=200,Beta=log(rf(K,m,m)),p=0)     # continuous data
-sim_data=simKnockoffGenotypes(n=200,Beta=log(rf(K,m,m)))  # SNP data
+#sim_data=simUnmatched(n=200,Beta=log(rf(K,m,m)),p=0)     # simulate continuous covariates
+sim_data=simKnockoffGenotypes(n=200,Beta=log(rf(K,m,m)))  # simulate SNP covariates
 pp=profilelkhd(data=sim_data,mvals=c(1:20),N=1000)
 
 save(pp,file=paste0("K=10_rep",n,".RData"))
+save(sim_data,file=paste0("data_K=10_",n,".RData"))
 
 
 
